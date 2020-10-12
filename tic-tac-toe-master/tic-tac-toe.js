1f192366634cec48b1f76963625b322cba69a697
Deniz Gould <deniz.gould@live.com>
2020-10-12 01:15:27 -0400

    added check for winner and update status functionality as well as game restart and cheating prevention

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -3,13 +3,18 @@
 // Add squares on click
 window.addEventListener("load", 
     function(){
-        document.getElementsByClassName('btn')[0].onclick = function () { 
-            const board = document.getElementById("board").children;
-            // console.log(board);
-            layoutBoard(board);
-            gameboard(board);
-            addHover(board);
 
+        let nullBoard = [ [null, null, null], [null, null, null], [null, null, null] ];
+        document.getElementsByClassName('btn')[0].onclick = function () { 
+
+            const board = document.getElementById("board").children;
+            layoutBoard(board);
+            gameboard(nullBoard, board);
+            addHover(board);
+            resetGameState(nullBoard, board);
+
+            // console.log(nullBoard);
+            console.log(board);
         }
     });
 
@@ -24,20 +29,17 @@ function layoutBoard(board) {
         // console.log(board[x]);
 
     }
-
 }
 
-function gameboard(board){
 
-    // let nullBoard = [ "","","","","","","","","" ];
 
-    // console.log(nullBoard)
+function gameboard(nullBoard, board){
 
     let player = true;
     for (let x = 0; x < board.length; x++){
         board[x].addEventListener("click", 
             function(){
-
+                // console.log(nullBoard)
                 let element = this;
 
                 if (player){
@@ -46,12 +48,16 @@ function gameboard(board){
                     player = !player;
                     let msg = document.getElementById("status").textContent 
                         = "It's Os Turn";
+                    placemark(nullBoard, "X", x);
+                    isWinner(nullBoard, "X");
                 }
                 else {
                     addO(element);
                     player = !player;
                     let msg = document.getElementById("status").textContent
                         = "It's Xs Turn";
+                    placemark(nullBoard, "O", x);
+                    isWinner(nullBoard, "O");
                 };
             // console.log("clicked");
             })
@@ -59,19 +65,19 @@ function gameboard(board){
     } 
 } 
 
-function addX(mark){
-
-    mark.classList.add("X");
-    mark.textContent = "X";
-
+function addX(square){
+    if (square.classList.contains('X')  || square.classList.contains('O') === false){
+    square.classList.add("X");
+    square.textContent = "X";
+    }
 }
 
-function addO(mark){
-
-    mark.classList.add("O");
-    mark.textContent = "O";
-
-}
+function addO(square){
+    if (square.classList.contains('O') || square.classList.contains('X') === false){
+        square.classList.add("O");
+        square.textContent = "O";
+        }
+    }
 
 function addHover(board){
     for (let x = 0; x < board.length; x++){
@@ -83,40 +89,118 @@ function addHover(board){
     }
 }
 
-// function placemark(nullboard, letter, x){
-//     switch (x) {
-//         case 0:
-//             nullboard[0] = letter;
-//             break;
-//         case 1:
-//             nullboard[1] = letter;
-//             break;
-//         case 2:
-//             nullboard[2] = letter;
-//             break;
-//         case 3:
-//             nullboard[3] = letter;
-//             break;
-//         case 4:
-//             nullboard[4] = letter;
-//             break;
-//         case 5:
-//             nullboard[5] = letter;
-//             break;
-//         case 6:
-//             nullboard[6] = letter;
-//             break;
-//         case 7:
-//             nullboard[7] = letter;
-//             break;
-//         case 8:
-//             nullboard[8] = letter;
-//             break;
+function placemark(nullboard, letter, index){
+    switch (index) {
+        case 0:
+            nullboard[0][0] = letter;
+            break;
+        case 1:
+            nullboard[0][1] = letter;
+            break;
+        case 2:
+            nullboard[0][2] = letter;
+            break;
+        case 3:
+            nullboard[1][0] = letter;
+            break;
+        case 4:
+            nullboard[1][1] = letter;
+            break;
+        case 5:
+            nullboard[1][2] = letter;
+            break;
+        case 6:
+            nullboard[2][0] = letter;
+            break;
+        case 7:
+            nullboard[2][1] = letter;
+            break;
+        case 8:
+            nullboard[2][2] = letter;
+            break;
 
-//     }
-// }
+    }
+}
+
+    const winningCombo = [
+        //rows
+        [ [0][0] ,[0][1] ,[0][2] ],
+        [ [1][0] ,[1][1] ,[1][2] ],
+        [ [2][0] ,[2][1] ,[2][2] ],
+        //cols
+        [ [0][0] ,[1][0] ,[2][0] ],
+        [ [0][1] ,[1][1] ,[2][1] ],
+        [ [0][2] ,[1][2] ,[2][2] ],
+        //diagonals
+        [ [0][0] ,[1][1] ,[2][2] ],
+        [ [0][2] ,[1][1] ,[2][0] ],
+    ];
 
 
+const rowValidate = (nullBoard, letter) => {
+    // console.log(nullBoard);
+    if(nullBoard[0][0]===letter && nullBoard[0][1]===letter && nullBoard[0][2]===letter){
+        return true;
+    }
+    else if (nullBoard[1][0]===letter && nullBoard[1][1]===letter && nullBoard[1][2]===letter){
+        return true;
+    }
+    else if (nullBoard[2][0]===letter && nullBoard[2][1]===letter && nullBoard[2][2]===letter){
+        return true;
+    }
+}
+
+const columnValidate = (nullBoard, letter) => {
+    if(nullBoard[0][0]===letter && nullBoard[1][0]===letter && nullBoard[2][0]===letter){
+        return true;
+    }
+    else if (nullBoard[0][1]===letter && nullBoard[1][1]===letter && nullBoard[2][1]===letter){
+        return true;
+    }
+    else if (nullBoard[0][2]===letter && nullBoard[1][2]===letter && nullBoard[2][2]===letter){
+        return true;
+    }
+}
+
+const diagValidate = (nullBoard, letter) => {
+    if(nullBoard[0][0]===letter && nullBoard[1][1]===letter && nullBoard[2][2]===letter){
+        return true;
+    }
+    else if (nullBoard[0][2]===letter && nullBoard[1][1]===letter && nullBoard[2][0]===letter){
+        return true;
+    }
+}
+
+function isWinner(nullBoard, letter){
+
+    if (rowValidate(nullBoard, letter)){
+        stateWinner(letter);
+
+    }else if (columnValidate(nullBoard, letter)){
+        stateWinner(letter);
+
+    }else if (diagValidate(nullBoard, letter)){
+        stateWinner(letter);
+        
+    }
+}
+
+function stateWinner(letter){
+    document.getElementById("status").textContent 
+                        = `Congratulations! ${letter} is the Winner!`;
+    document.getElementById("status").classList.add("you-won");   
+
+    // let y = document.getElementById("status").textContent 
+                        // = `Congratulations! ${letter} is the Winner!`;
+    // console.log(y)        
+}
 
 
+function resetGameState (nullBoard, board){
+    document.getElementById("status").textContent = "Move your mouse over a square and click to play an X or an O."
+    nullBoard = [ [null, null, null], [null, null, null], [null, null, null] ];
+    document.getElementById("status").classList.remove("you-won");           
+    layoutBoard(board);
 
+    console.log(nullBoard);
+}
