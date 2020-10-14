83e23fe3049d19463de2edcb7e1cb899bbdcacab
Deniz Gould <deniz.gould@live.com>
2020-10-13 22:55:38 -0400

    fixed last bugs

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -1,19 +1,20 @@
 "use strict"
+var nullBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+var winner = false;
 
 // Add squares on click
-window.addEventListener("load", 
-    function(){
-
-        let nullBoard = [ [null, null, null], [null, null, null], [null, null, null] ];
-        document.getElementsByClassName('btn')[0].onclick = function () { 
-
-            const board = document.getElementById("board").children;
-            layoutBoard(board);
-            gameboard(nullBoard, board);
-            addHover(board);
-            resetGameState(nullBoard, board);
-
-            // console.log(nullBoard);
+window.addEventListener("load",
+    function() {
+        const board = document.getElementById("board").children;
+        layoutBoard(board);
+        gameboard(board);
+        addHover(board);
+        document.getElementsByClassName('btn')[0].onclick = function() {
+            resetGameState(board);
             console.log(board);
         }
     });
@@ -32,175 +33,211 @@ function layoutBoard(board) {
 }
 
 
+function playerChange(currentPlayer) {
+    if (currentPlayer === 'X') {
+        return 'O';
+    } else {
+        return 'X';
+    }
+}
 
-function gameboard(nullBoard, board){
+function gameboard(board) {
 
-    let player = true;
-    for (let x = 0; x < board.length; x++){
-        board[x].addEventListener("click", 
-            function(){
-                // console.log(nullBoard)
+    let player = playerChange('O')
+    for (let x = 0; x < board.length; x++) {
+        board[x].addEventListener("click",
+            function() {
+                console.log(nullBoard)
                 let element = this;
-
-                if (player){
-
-                    addX(element);
-                    player = !player;
-                    let msg = document.getElementById("status").textContent 
-                        = "It's Os Turn";
-                    placemark(nullBoard, "X", x);
-                    isWinner(nullBoard, "X");
+                if (element.innerHTML === "" && winner === false) {
+                    document.getElementById("status").textContent = `It's ${player}'s turn`;
+                    if (player === 'X') {
+                        addX(element);
+                        placemark("X", x);
+                        isWinner("X");
+                        player = playerChange('X');
+                    } else {
+                        addO(element);
+                        placemark("O", x);
+                        isWinner("O");
+                        player = playerChange('O');
+                    };
                 }
-                else {
-                    addO(element);
-                    player = !player;
-                    let msg = document.getElementById("status").textContent
-                        = "It's Xs Turn";
-                    placemark(nullBoard, "O", x);
-                    isWinner(nullBoard, "O");
-                };
-            // console.log("clicked");
+                // console.log("clicked");
             })
 
-    } 
-} 
-
-function addX(square){
-    if (square.classList.contains('X')  || square.classList.contains('O') === false){
-    square.classList.add("X");
-    square.textContent = "X";
     }
 }
 
-function addO(square){
-    if (square.classList.contains('O') || square.classList.contains('X') === false){
+function addX(square) {
+    if (square.classList.contains('X') || square.classList.contains('O') === false) {
+        square.classList.add("X");
+        square.textContent = "X";
+    }
+
+}
+
+function addO(square) {
+    if (square.classList.contains('O') || square.classList.contains('X') === false) {
         square.classList.add("O");
         square.textContent = "O";
-        }
-    }
-
-function addHover(board){
-    for (let x = 0; x < board.length; x++){
-        board[x].addEventListener("mouseover", function(){
-            board[x].classList.add("hover")});
-            
-            board[x].addEventListener("mouseout", function(){
-                board[x].classList.remove("hover")});
     }
 }
 
-function placemark(nullboard, letter, index){
+function addHover(board) {
+    for (let x = 0; x < board.length; x++) {
+        board[x].addEventListener("mouseover", function() {
+            board[x].classList.add("hover")
+        });
+
+        board[x].addEventListener("mouseout", function() {
+            board[x].classList.remove("hover")
+        });
+    }
+}
+
+function placemark(letter, index) {
     switch (index) {
         case 0:
-            nullboard[0][0] = letter;
+            nullBoard[0][0] = letter;
             break;
         case 1:
-            nullboard[0][1] = letter;
+            nullBoard[0][1] = letter;
             break;
         case 2:
-            nullboard[0][2] = letter;
+            nullBoard[0][2] = letter;
             break;
         case 3:
-            nullboard[1][0] = letter;
+            nullBoard[1][0] = letter;
             break;
         case 4:
-            nullboard[1][1] = letter;
+            nullBoard[1][1] = letter;
             break;
         case 5:
-            nullboard[1][2] = letter;
+            nullBoard[1][2] = letter;
             break;
         case 6:
-            nullboard[2][0] = letter;
+            nullBoard[2][0] = letter;
             break;
         case 7:
-            nullboard[2][1] = letter;
+            nullBoard[2][1] = letter;
             break;
         case 8:
-            nullboard[2][2] = letter;
+            nullBoard[2][2] = letter;
             break;
 
     }
 }
 
-    const winningCombo = [
-        //rows
-        [ [0][0] ,[0][1] ,[0][2] ],
-        [ [1][0] ,[1][1] ,[1][2] ],
-        [ [2][0] ,[2][1] ,[2][2] ],
-        //cols
-        [ [0][0] ,[1][0] ,[2][0] ],
-        [ [0][1] ,[1][1] ,[2][1] ],
-        [ [0][2] ,[1][2] ,[2][2] ],
-        //diagonals
-        [ [0][0] ,[1][1] ,[2][2] ],
-        [ [0][2] ,[1][1] ,[2][0] ],
-    ];
+const winningCombo = [
+    //rows
+    [
+        [0][0],
+        [0][1],
+        [0][2]
+    ],
+    [
+        [1][0],
+        [1][1],
+        [1][2]
+    ],
+    [
+        [2][0],
+        [2][1],
+        [2][2]
+    ],
+    //cols
+    [
+        [0][0],
+        [1][0],
+        [2][0]
+    ],
+    [
+        [0][1],
+        [1][1],
+        [2][1]
+    ],
+    [
+        [0][2],
+        [1][2],
+        [2][2]
+    ],
+    //diagonals
+    [
+        [0][0],
+        [1][1],
+        [2][2]
+    ],
+    [
+        [0][2],
+        [1][1],
+        [2][0]
+    ],
+];
 
 
-const rowValidate = (nullBoard, letter) => {
-    // console.log(nullBoard);
-    if(nullBoard[0][0]===letter && nullBoard[0][1]===letter && nullBoard[0][2]===letter){
+const rowValidate = (letter) => {
+    if (nullBoard[0][0] === letter && nullBoard[0][1] === letter && nullBoard[0][2] === letter) {
         return true;
-    }
-    else if (nullBoard[1][0]===letter && nullBoard[1][1]===letter && nullBoard[1][2]===letter){
+    } else if (nullBoard[1][0] === letter && nullBoard[1][1] === letter && nullBoard[1][2] === letter) {
         return true;
-    }
-    else if (nullBoard[2][0]===letter && nullBoard[2][1]===letter && nullBoard[2][2]===letter){
+    } else if (nullBoard[2][0] === letter && nullBoard[2][1] === letter && nullBoard[2][2] === letter) {
         return true;
     }
 }
 
-const columnValidate = (nullBoard, letter) => {
-    if(nullBoard[0][0]===letter && nullBoard[1][0]===letter && nullBoard[2][0]===letter){
+const columnValidate = (letter) => {
+    if (nullBoard[0][0] === letter && nullBoard[1][0] === letter && nullBoard[2][0] === letter) {
         return true;
-    }
-    else if (nullBoard[0][1]===letter && nullBoard[1][1]===letter && nullBoard[2][1]===letter){
+    } else if (nullBoard[0][1] === letter && nullBoard[1][1] === letter && nullBoard[2][1] === letter) {
         return true;
-    }
-    else if (nullBoard[0][2]===letter && nullBoard[1][2]===letter && nullBoard[2][2]===letter){
-        return true;
-    }
-}
-
-const diagValidate = (nullBoard, letter) => {
-    if(nullBoard[0][0]===letter && nullBoard[1][1]===letter && nullBoard[2][2]===letter){
-        return true;
-    }
-    else if (nullBoard[0][2]===letter && nullBoard[1][1]===letter && nullBoard[2][0]===letter){
+    } else if (nullBoard[0][2] === letter && nullBoard[1][2] === letter && nullBoard[2][2] === letter) {
         return true;
     }
 }
 
-function isWinner(nullBoard, letter){
+const diagValidate = (letter) => {
+    if (nullBoard[0][0] === letter && nullBoard[1][1] === letter && nullBoard[2][2] === letter) {
+        return true;
+    } else if (nullBoard[0][2] === letter && nullBoard[1][1] === letter && nullBoard[2][0] === letter) {
+        return true;
+    }
+}
 
-    if (rowValidate(nullBoard, letter)){
+function isWinner(letter) {
+    console.log(letter)
+    if (rowValidate(letter)) {
         stateWinner(letter);
+        winner = true;
 
-    }else if (columnValidate(nullBoard, letter)){
+    } else if (columnValidate(letter)) {
         stateWinner(letter);
+        winner = true;
 
-    }else if (diagValidate(nullBoard, letter)){
+    } else if (diagValidate(letter)) {
         stateWinner(letter);
-        
+        winner = true;
+
     }
 }
 
-function stateWinner(letter){
-    document.getElementById("status").textContent 
-                        = `Congratulations! ${letter} is the Winner!`;
-    document.getElementById("status").classList.add("you-won");   
+function stateWinner(letter) {
+    document.getElementById("status").textContent = `Congratulations! ${letter} is the Winner!`;
+    document.getElementById("status").classList.add("you-won");
 
     // let y = document.getElementById("status").textContent 
-                        // = `Congratulations! ${letter} is the Winner!`;
-    // console.log(y)        
+    // = `Congratulations! ${letter} is the Winner!`;    
 }
 
 
-function resetGameState (nullBoard, board){
+function resetGameState(board) {
     document.getElementById("status").textContent = "Move your mouse over a square and click to play an X or an O."
-    nullBoard = [ [null, null, null], [null, null, null], [null, null, null] ];
-    document.getElementById("status").classList.remove("you-won");           
+    nullBoard = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    document.getElementById("status").classList.remove("you-won");
     layoutBoard(board);
-
-    console.log(nullBoard);
+    winner = false;
 }
